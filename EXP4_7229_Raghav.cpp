4f5ca5721703812c93aebfe10a10bb44f76a9a8b
Xx-Raghav-xX <161009205+Xx-Raghav-xX@users.noreply.github.com>
2024-03-16 20:02:18 +0530

    Create EXP4_7229_Raghav.cpp

---
+++ EXP4_7229_Raghav.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+ 
+class Number
+{
+
+public:
+Number (int num):num (num)
+  {
+  } 
+ 
+ 
+void decimal_to_binary ()
+  {
+	
+if (num == 0)
+	  {
+		
+std::cout << 0;
+		
+return;
+	  
+}
+	
+char binary[32];
+	
+int index = 0;
+	
+while (num > 0)
+	  {
+		
+binary[index++] = num % 2 ? '1' : '0';
+		
+num /= 2;
+	  
+}
+	
+ 
+for (int i = index - 1; i >= 0; i--)
+	  {
+		
+std::cout << binary[i];
+  
+} 
+} 
+ 
+ 
+int num;
+
+};
+
+
+ 
+int
+main ()
+{
+  
+int numb;
+  
+cin >> numb;
+  cout << "Enter the number " << endl;
+Number number (numb);
+  
+std::cout << "The binary equivalent of " << number.num << " is ";
+  
+number.decimal_to_binary ();
+  
+std::cout << std::endl;
+  
+return 0;
+
+}
+
+
