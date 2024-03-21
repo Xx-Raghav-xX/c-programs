fe0c4ab9fe162a80ce271ec5567432bb254dc478
Xx-Raghav-xX <161009205+Xx-Raghav-xX@users.noreply.github.com>
2024-03-21 14:53:15 +0530

    Update EXP3_7229_Raghav_D2.cpp
    Create a class Room with Data members: length, breadth. Include functions get () and display () to get and display the data of room. Include two additional functions: area () and perimeter () to calculate the area and perimeter of the room. Implement the class by creating at least 5 objects of the class and display the area
    and perimeter of all 5 rooms.

--- EXP3_7229_Raghav_D2.cpp
+++ EXP3_7229_Raghav_D2.cpp
@@ -1,3 +1,7 @@
+//Create a class Room with Data members: length, breadth. Include functions get () and display () to get
+//and display the data of room. Include two additional functions: area () and perimeter () to calculate the area
+//and perimeter of the room. Implement the class by creating at least 5 objects of the class and display the area
+//and perimeter of all 5 rooms.
 #include<iostream>
 using namespace std;
 
@@ -67,7 +71,7 @@ cout << "the perimeter is ";
 int
 main () 
  { 
-room obj1;
+room obj1 , ob2 , ob3 , ob4 , ob5;
   
 obj1.get ();
   
@@ -76,6 +80,59 @@ obj1.display ();
 obj1.area ();
   
 obj1.perimeter ();
+
+//--------------------------------------------------
+
+obj2.get ();
+  
+obj2.display ();
+  
+obj2.area ();
+  
+obj2.perimeter ();
+
+//--------------------------------------------------
+
+obj3.get ();
+  
+obj3.display ();
+  
+obj3.area ();
+  
+obj3.perimeter ();
+
+
+//--------------------------------------------------
+
+obj3.get ();
+  
+obj3.display ();
+  
+obj3.area ();
+  
+obj3.perimeter ();
+
+
+//--------------------------------------------------
+
+obj4.get ();
+  
+obj4.display ();
+  
+obj4.area ();
+  
+obj4.perimeter ();
+
+
+//--------------------------------------------------
+
+obj5.get ();
+  
+obj5.display ();
+  
+obj5.area ();
+  
+obj5.perimeter ();
   
  
 return 0;
